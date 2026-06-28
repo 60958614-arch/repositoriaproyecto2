c7e0da1f9ef2c6e01b092ca539800030e0299d77
cristianhuari <77380393@continental.edu.pe>
2026-06-27 20:47:18 -0500

    Describir brevemente el cambio realizado

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ dni = int(input("ingrese DNI del cliente :\t\t\t"))
 apellido = input("Ingrese apellido del cliente :\t\t")
 nombre = input("ingrese nombre del cliente:\t\t\t")
 DatosCompletos = nombre + " " + apellido  
+print("*************** Tipo de cambio  ****************")
+Tipodecambio = float(input("Ingrese tipo de cambio a SOLES:\t\t"))
+
+#calculando
+MontoimpuestoDolares = preciodeventaendolares * 0.19
+MontopagoDolares = preciodeventaendolares + MontoimpuestoDolares
+
+MontoInpuestoSoles = MontoimpuestoDolares * Tipodecambio
+MontoPagoSoles = MontopagoDolares * Tipodecambio
