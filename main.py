36d6addac1c2048fa9289991ce3f7260fd4b66c2
cristianhuari <77380393@continental.edu.pe>
2026-06-27 20:34:35 -0500

    proyecto1

---
+++ main.py
@@ -0,0 +1,8 @@
+from datetime import datetime 
+
+print("\n=================== INGRESO DE DATOS ======================")
+print("************** Cliente : ************")
+dni = int(input("ingrese DNI del cliente :\t\t\t"))
+apellido = input("Ingrese apellido del cliente :\t\t")
+nombre = input("ingrese nombre del cliente:\t\t\t")
+DatosCompletos = nombre + " " + apellido  
